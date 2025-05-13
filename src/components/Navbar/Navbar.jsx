6a7f95b22c07f467d9bdb257ce9e2bd49fd9bf56
olivialrj <olivialrj@hotmail.com>
2025-05-12 22:02:48 -0700

    Updated NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -182,14 +182,14 @@ function Navbar() {
         <div className="space-y-1 px-2 pt-2 pb-3">
           {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
           <Link
-            href="#"
+            to="/"
             className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
             aria-current="page"
           >
             About
           </Link>
           <Link
-            href="#"
+            to="/projects"
             className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
           >
             Projects
